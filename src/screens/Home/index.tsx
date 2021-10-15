fb0a0bf5f47626c15b4a7e7f77b70dad9c1b0b9f
Guilherme Dias <guilhermehenriquedias@hotmail.com>
2021-10-15 08:59:35 -0300

    adding card component for cars and backbutton

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,9 +4,28 @@ import { StatusBar } from "expo-status-bar";
 
 import Logo from "../../assets/logo.svg";
 
-import { Container, Header, HeaderContent, TotalCars } from "./styles";
+import {
+  CarList,
+  Container,
+  Header,
+  HeaderContent,
+  TotalCars,
+  Carlist
+} from "./styles";
+import Car from "../../components/Car";
 
 const Home: React.FC = () => {
+  const carData = {
+    brand: "Audi",
+    name: "RS 5 Coupé",
+    rent: {
+      period: "Ao dia",
+      price: 120
+    },
+    thumbnail:
+      "https://production.autoforce.com/uploads/version/profile_image/3188/model_main_comprar-tiptronic_87272c1ff1.png"
+  };
+
   return (
     <Container>
       <StatusBar style="light" backgroundColor="transparent" translucent />
@@ -17,6 +36,13 @@ const Home: React.FC = () => {
           <TotalCars>Total de 12 carros</TotalCars>
         </HeaderContent>
       </Header>
+
+      {/* @ts-ignore */}
+      <CarList
+        data={[1, 2, 3]}
+        keyExtractor={(item) => String(item)}
+        renderItem={({ item }) => <Car data={carData} />}
+      />
     </Container>
   );
 };
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,5 +1,7 @@
+import { FlatList } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
+import { CarData } from "../../components/Car";
 
 export const Container = styled.View`
   flex: 1;
@@ -26,3 +28,10 @@ export const TotalCars = styled.Text`
   font-family: ${({ theme }) => theme.fonts.primary_regular};
   color: ${({ theme }) => theme.colors.text};
 `;
+
+export const CarList = styled(FlatList as new () => FlatList<CarData>).attrs({
+  contentContainerStyle: {
+    padding: 24
+  },
+  showsVerticalScrollIndicator: false
+})``;
