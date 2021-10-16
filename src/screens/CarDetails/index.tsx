bbae984d385b3153af94dcdcc8e953b2d6a73d86
Guilherme Dias <guilhermehenriquedias@hotmail.com>
2021-10-16 12:34:27 -0300

    adding cardetails layout

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import Accessory from "../../components/Accessory";
 import BackButton from "../../components/BackButton";
+import ImageSlider from "../../components/ImageSlider";
 
-import { Container, Header } from "./styles";
+import SpeedSvg from "../../assets/speed.svg";
+import AccelerationSvg from "../../assets/acceleration.svg";
+import ForceSvg from "../../assets/force.svg";
+import GasolineSvg from "../../assets/gasoline.svg";
+import ExchangeSvg from "../../assets/exchange.svg";
+import PeopleSvg from "../../assets/people.svg";
+
+import {
+  Container,
+  Header,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+  Acessories,
+  Footer
+} from "./styles";
+import Button from "../../components/Button";
 
 const CarDetails: React.FC = () => {
   return (
@@ -9,6 +33,46 @@ const CarDetails: React.FC = () => {
       <Header>
         <BackButton onPress={() => {}} />
       </Header>
+
+      <ImageSlider
+        imagesUrl={[
+          "https://production.autoforce.com/uploads/version/profile_image/3188/model_main_comprar-tiptronic_87272c1ff1.png"
+        ]}
+      />
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 350</Price>
+          </Rent>
+        </Details>
+
+        <Acessories>
+          <Accessory name="380 km/h" icon={SpeedSvg} />
+          <Accessory name="3.2s" icon={AccelerationSvg} />
+          <Accessory name="800 HP" icon={ForceSvg} />
+          <Accessory name="Gasolina" icon={GasolineSvg} />
+          <Accessory name="Auto" icon={ExchangeSvg} />
+          <Accessory name="2 pessoas" icon={PeopleSvg} />
+        </Acessories>
+
+        <About>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod
+          laboriosam culpa accusamus eaque, assumenda molestias quaerat quas
+          neque dolor esse iste officiis provident, vitae fugiat repellat vero,
+          saepe nulla itaque.
+        </About>
+      </Content>
+
+      <Footer>
+        <Button title="Confirmar" />
+      </Footer>
     </Container>
   );
 };
